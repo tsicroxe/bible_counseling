0b90a2c1c6027be87ea70880d0854f58e112b2b5
tsicroxE <arbanakus@gmail.com>
2018-02-01 10:07:36 -0800

    took out mongoose for commit

--- server.js
+++ server.js
@@ -18,12 +18,9 @@ app.use(bodyParser.json());
 // End Middleware
 
 
-// set an environment variable called APPROOT to keep track of the root folder of your app
   process.env['APPROOT'] = __dirname;
 
-  //require mongoose configuration, use path.join to build the route
-  // require(path.join(process.env['APPROOT'], 'server/config/mongoose.js'));
-  //require routes configuration, get a function from the module.exports, that gets invoked while passing it the app
+
   require(path.join(process.env['APPROOT'], 'server/config/routes.js'))(app);
 
 var server = app.listen(PORT, function(){
